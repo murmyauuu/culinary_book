2fef33171abd2c707609d2a6a81b76585d5ce9a8
WorkD69 <artm.malyshev.2006@inbox.ru>
2025-12-23 01:41:18 +0300

    Добавил валидации в RecipeForm, улучшил модель

--- recipes/forms.py
+++ recipes/forms.py
@@ -6,3 +6,15 @@ class RecipeForm(forms.ModelForm):
     class Meta:
         model = Recipe
         fields = ["title", "description", "cooking_time_minutes"]
+
+    def clean_cooking_time_minutes(self):
+        value = self.cleaned_data.get("cooking_time_minutes")
+        if value and value < 1:
+            raise forms.ValidationError("Время приготовления должно быть положительным числом.")
+        return value
+
+    def clean_title(self):
+        title = self.cleaned_data.get("title")
+        if len(title) < 3:
+            raise forms.ValidationError("Название должно содержать не менее 3 символов.")
+        return title
